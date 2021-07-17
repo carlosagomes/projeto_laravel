39ca5105a1212db17ddfc27f87a1df2771f83ddf
Carlos Gomes <gomescarlos93ale@gmail.com>
2021-07-17 14:10:08 -0300

    Ajustes para deixar responsivo

--- public/front/index.php
+++ public/front/index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -25,7 +26,7 @@
                     </div>
                     <div class="panel-body">
                         <div class="row">
-                            <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="col-md-12 col-sm-12 col-xs-12 table-responsive">
                             <table class="table table-bordered">
                                 <thead>
                                     <tr>
